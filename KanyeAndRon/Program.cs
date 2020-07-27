d62de5fea8325e260b5416fa845da152dfc23043
Niokhobaye Diouf <66567989+nioks23@users.noreply.github.com>
2020-07-27 16:02:54 -0400

    Quote Generator created

---
+++ KanyeAndRon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ KanyeAndRon/QuoteGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRon
+{
+    public class QuoteGenerator
+    {
+        public QuoteGenerator()
+        {
+        }
+
+        public static void KanyeQuotes()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"Kanye says: {kanyeQuote}");
+            Console.WriteLine("--------------------------------------");
+        }
+
+        public static void RonQuotes()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            
+            Console.WriteLine($"Ron responds with: {ronQuote}");
+            Console.WriteLine("--------------------------------------");
+
+
+        }
+    }
+}
